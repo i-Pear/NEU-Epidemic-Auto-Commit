c78a590433b2f76f2197c6f1392a902484a2c2c9
iPear <434317543@qq.com>
2020-02-16 10:46:20 +0800

    optimized for poor net connection

--- automation.py
+++ automation.py
@@ -1,3 +1,5 @@
+# coding:utf-8
+
 """
 Made by @i.Pear 2020/02/15
 """
@@ -15,30 +17,51 @@ firstPassword = ""
 def run():
     driver.get('http://stuinfo.neu.edu.cn/')
     time.sleep(2)
+    WebDriverWait(driver, 10).until(lambda x:
+                                    len(x.find_elements_by_xpath(
+                                        '//*[@id="app"]/div/div[1]/div[3]/div[2]/div[1]/input')) > 0)
     in1 = driver.find_element_by_xpath('//*[@id="app"]/div/div[1]/div[3]/div[2]/div[1]/input')
     in1.click()
     in1.clear()
     in1.send_keys(stuID)
+    WebDriverWait(driver, 10).until(lambda x:
+                                    len(x.find_elements_by_xpath(
+                                        '//*[@id="app"]/div/div[1]/div[3]/div[2]/div[2]/div/input')) > 0)
     in2 = driver.find_element_by_xpath('//*[@id="app"]/div/div[1]/div[3]/div[2]/div[2]/div/input')
     in2.click()
     in2.clear()
     in2.send_keys(firstPassword)
+    WebDriverWait(driver, 10).until(
+        lambda x: len(x.find_elements_by_xpath('//*[@id="app"]/div/div[1]/div[3]/button')) > 0)
     driver.find_element_by_xpath('//*[@id="app"]/div/div[1]/div[3]/button').click()
     time.sleep(2)
+    WebDriverWait(driver, 60).until(
+        lambda x: len(x.find_elements_by_xpath('//*[ @id="app"]/section/aside/div/ul/li[2]')) > 0)
     print('Platfrom logined successfully.')
     # 平台登陆完成
 
     # 转向疫情页面
     driver.get('http://stuinfo.neu.edu.cn/#/studentPort/serveAdmin')
+    WebDriverWait(driver, 60).until(lambda x: len(x.find_elements_by_xpath(
+        '//*[@id="app"]/section/section/section/section/main/div[1]/div[1]/div/div[2]/div/div/div/div[2]')) > 0
+                                              and x.find_element_by_xpath(
+        '//*[@id="app"]/section/section/section/section/main/div[1]/div[1]/div/div[2]/div/div/div/div[2]').text == '东北大学学生防控信息统计系统')
+    WebDriverWait(driver, 60).until(lambda x: len(x.find_elements_by_xpath(
+        '//*[@id="app"]/section/section/section/section/main/div[1]/div[1]/div/div[2]/div/div/div')) > 0)
     time.sleep(2)
     print('Getting to the info site...')
     driver.find_element_by_xpath(
         '//*[@id="app"]/section/section/section/section/main/div[1]/div[1]/div/div[2]/div/div/div').click()
-    time.sleep(3)
-    # 重定向完成
+    time.sleep(2)
+    while len(driver.window_handles) == 1:
+        time.sleep(5)
+        driver.find_element_by_xpath(
+            '//*[@id="app"]/section/section/section/section/main/div[1]/div[1]/div/div[2]/div/div/div').click()
+        # 重定向完成
 
     # 开始填写信息
     driver.switch_to.window(driver.window_handles[1])
+    WebDriverWait(driver, 60).until(lambda x: len(x.find_elements_by_name('sfgcyiqz')) > 0)
     box = driver.find_element_by_name('sfgcyiqz')
     box = Select(box)
     box.select_by_value('否')
