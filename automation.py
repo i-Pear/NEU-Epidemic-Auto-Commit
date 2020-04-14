e46a19e2bf2b349c6d6f0d5e1a9923a040fbf764
iPear <434317543@qq.com>
2020-04-14 20:57:22 +0800

    Update automation.py

--- automation.py
+++ automation.py
@@ -48,29 +48,39 @@ def run():
     # 平台登陆完成
 
     # 开始填写信息
+    time.sleep(5)
     WebDriverWait(driver, 60).until(lambda x: len(
-        x.find_elements_by_xpath('//*[@id="app"]/main/div/form/div[1]/table/tbody/tr/td[1]/div/div/div/label[1]')) > 0)
+        x.find_elements_by_xpath(
+            '//*[@id="app"]/main/div/form/div[1]/table/tbody/tr/td[1]/div/div/div/label[1]/span[1]/span')) > 0)
     driver.find_element_by_xpath(
-        '//*[@id="app"]/main/div/form/div[1]/table/tbody/tr/td[1]/div/div/div/label[1]').click()
+        '//*[@id="app"]/main/div/form/div[1]/table/tbody/tr/td[1]/div/div/div/label[1]/span[1]/span').click()
 
+    time.sleep(5)
     WebDriverWait(driver, 60).until(lambda x: len(x.find_elements_by_xpath(
-        '//*[@id="app"]/main/div/form/div[3]/div[2]/table/tbody/tr[1]/td/div/div/div/label[1]')) > 0)
+        '//*[@id="app"]/main/div/form/div[3]/div[2]/table/tbody/tr/td/div/div/div/label[1]/span[1]/span')) > 0)
     driver.find_element_by_xpath(
-        '//*[@id="app"]/main/div/form/div[3]/div[2]/table/tbody/tr[1]/td/div/div/div/label[1]').click()
+        '//*[@id="app"]/main/div/form/div[3]/div[2]/table/tbody/tr/td/div/div/div/label[1]/span[1]/span').click()
 
+    time.sleep(5)
     WebDriverWait(driver, 60).until(lambda x: len(x.find_elements_by_xpath(
-        '//*[@id="app"]/main/div/form/div[4]/div[2]/table/tbody/tr[1]/td/div/div/div/label[1]')) > 0)
+        '//*[@id="app"]/main/div/form/div[4]/div[2]/table/tbody/tr[1]/td/div/div/div/label[1]/span[1]/span')) > 0)
     driver.find_element_by_xpath(
-        '//*[@id="app"]/main/div/form/div[4]/div[2]/table/tbody/tr[1]/td/div/div/div/label[1]').click()
+        '//*[@id="app"]/main/div/form/div[4]/div[2]/table/tbody/tr[1]/td/div/div/div/label[1]/span[1]/span').click()
 
-    time.sleep(1)
+    time.sleep(5)
     WebDriverWait(driver, 60).until(lambda x: len(x.find_elements_by_xpath(
         '//*[@id="app"]/main/div/form/div[6]/button')) > 0)
     driver.find_element_by_xpath('//*[@id="app"]/main/div/form/div[6]/button').click()
 
+    driver.save_screenshot('1.png')
+    time.sleep(2)
+    time.sleep(5)
+    windows = driver.window_handles
+    print('#debug: windows count=', len(windows))
     WebDriverWait(driver, 30).until(
-        lambda x: len(x.find_elements_by_xpath('//*[@id="app"]/main/div/div/div/div/div[2]/a[1]')) > 0)
-
+        lambda x: len(x.find_elements_by_xpath('//*[@id="app"]/main/div/div/div/div/div[1]')) > 0)
+    if driver.find_element_by_xpath('//*[@id="app"]/main/div/div/div/div/div[1]').text != "上报成功":
+        raise Exception("Unknown ERROR")
     # 填写信息结束
 
 
@@ -79,7 +89,7 @@ if __name__ == '__main__':
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument("window-size=1024,768")
+    chrome_options.add_argument("window-size=1920,1080")
     chrome_options.add_argument("--no-sandbox")
 
     driver = webdriver.Chrome(chrome_options=chrome_options)
